aa7eab1a05d2281abf5d40313c56f4171e086d86
youjunsang <junsangy1234@gmail.com>
2026-03-06 17:27:04 +0900

    JDBC and JPA 스프링부트 DB 연동

--- hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,22 +1,43 @@
 package hello.hello_spring;
-
+import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
-import org.springframework.beans.factory.annotation.Configurable;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    private final MemberRepository memberRepository;
+
+    @Autowired //생성자 하나인 경우는 @Autowired 생략 가능하긴 함
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+    //JpaMemberRepository 사용
+    /*@PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }*/
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
+
+    //JpaMemberRepository 사용
+    /*@Bean
+    public MemberRepository memberRepository() {
+        //return new MemoryMemberRepository();
+        //return new JpaMemberRepository(em);
+    }*/
 }
--- hello-spring/src/main/java/hello/hello_spring/domain/Member.java
+++ hello-spring/src/main/java/hello/hello_spring/domain/Member.java
@@ -1,7 +1,13 @@
 package hello.hello_spring.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //DB TABLE에서 자동으로 아이디 생성되는 형식을 IDENTITY라고 함
     private Long id;
+
     private String name;
 
     public Long getId() {
